bf7189bf5b7c43be6ff0a3b057b9d06582623ff4
daressel <nevaliashkaoxxxy@gmail.com>
2024-02-26 22:40:55 +0300

    add use pathname for active tab

--- src/layouts/Menu/Sidebar.tsx
+++ src/layouts/Menu/Sidebar.tsx
@@ -1,24 +1,22 @@
+'use client';
 import { FC, memo } from 'react';
 
 import { Tab, Tabs, Typography } from '@mui/material';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 type TSidebar = {};
 
 const Component: FC<TSidebar> = () => {
+  const pathname = usePathname();
   return (
     <>
       <Typography align="center">Menu</Typography>
-      <Tabs value="Categories" orientation="vertical" variant="scrollable">
-        <Link href="/categories">
-          <Tab value="Categories" label="Categories" />
-        </Link>
-        <Link href="/products">
-          <Tab value="Products" label="Products" />
-        </Link>
-        <Link href="/about-us">
-          <Tab value="About Us" label="About Us" />
-        </Link>
+      <Tabs value={pathname} orientation="vertical" variant="scrollable">
+        <Tab label="Main" value="/" href="/" component={Link} />
+        <Tab label="Categories" value="/categories" href="/categories" component={Link} />
+        <Tab label="Products" value="/products" href="/products" component={Link} />
+        <Tab label="About us" value="/about-us" href="/about-us" component={Link} />
       </Tabs>
     </>
   );
